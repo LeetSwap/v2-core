a2ca678f01ac937908750b989addc0a355c9e30f
Gilgames000 <22778436+Gilgames000@users.noreply.github.com>
2023-08-13 22:53:51 +0200

    feat: add script to get init hash

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,5 +1,9 @@
 import { task } from 'hardhat/config'
 import '@nomiclabs/hardhat-ethers'
+// import { UniswapV2Factory } from '../dist/types'
+// import UniswapV2Pair from '../build/UniswapV2Pair.json'
+import { utils } from 'ethers'
+import { bytecode } from "../artifacts/contracts/UniswapV2Pair.sol/UniswapV2Pair.json";
 import { Logger } from 'tslog'
 import config from './config/config'
 
@@ -14,3 +18,20 @@ task('deploy-factory', 'Deploys UniswapV2Factory contract')
 
         logger.info(instance.address)
     })
+
+// task('get-hash', 'Get pair hash')
+//     .setAction(async (args, hre) => {
+//         const pairCodeHash = hre.ethers.utils.keccak256(`0x${UniswapV2Pair.evm.bytecode.object}`)
+//         console.log(`Pair code hash: ${pairCodeHash}`)
+//
+//         // const instance = await hre.ethers.getContractAt("UniswapV2Factory", "0x05df80f2B35daB65C03B080031c520447F9cce14") as UniswapV2Factory
+//
+//         // const initCodeHash = await instance.pairCodeHash()
+//         // logger.info(initCodeHash)
+//     })
+
+task('init-hash', '')
+    .setAction(async (args, hre) => {
+        logger.info(utils.keccak256(bytecode))
+        logger.info(utils.solidityKeccak256(['bytes'], [bytecode]))
+    });
